aa9170dba4cf85bec26c13df3fcaab9c4b6d8213
NatsukiYu <tsgr.natsuki.yu@gmail.com>
2021-09-03 16:01:15 +0900

    create setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt', 'r') as fp:
+    requirements = fp.read().splitlines()
+
+setup(
+    name='IconCog',
+    version='1.0.0',
+    license='MIT',
+    description='Send Husan.',
+    author='@natsuki__yu',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=requirements,
+    include_package_data=True,
+)
